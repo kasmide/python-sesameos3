f2fe81720f1611fe86863a588c324956c063457b
kasmide <email@ksmd.dev>
2025-05-31 05:34:19 +0900

    export more types

--- sesameos3client/__init__.py
+++ sesameos3client/__init__.py
@@ -1,8 +1,9 @@
-from .sesame_client import SesameClient, Event
+from .sesame_client import SesameClient, Event, EventData
 
 __version__ = "0.1.0"
 
 __all__ = [
     "SesameClient",
     "Event",
+    "EventData",
 ]
